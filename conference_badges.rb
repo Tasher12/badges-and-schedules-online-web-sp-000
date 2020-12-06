f7f79642f3a5c396373a96c3c0768f50cd659547
authentic-pipe-8414 <tasher.stidham@outlook.com>
2020-12-06 17:24:02 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,7 +1,7 @@
 # Write your code here.
 def badge_maker(name)
   name = []
-  name.each {|names| puts "Hello, my name is #{names}."}
+  name.each{|names| puts "Hello, my name is #{names}."
   name << names
 end
 
