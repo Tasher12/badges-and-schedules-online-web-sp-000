dadfc71e6df57a65184ee65a121b91d7bcf46db6
authentic-pipe-8414 <tasher.stidham@outlook.com>
2020-12-06 17:47:27 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,8 +5,9 @@ def badge_maker(name)
   return "Hello, my name is #{name}." 
 end
 
-def batch_badge_creator(names)
-  returns badge_messages
+def batch_badge_creator(attendees)
+  attendees = Array.new 
+  return attendees
 end
 
 def assign_rooms(speakers)
